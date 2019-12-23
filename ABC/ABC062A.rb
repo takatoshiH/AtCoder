276c1462d6e9912861c6fefeeadf457ccaffbe47
takatoshiH <t.hikijiri@gmail.com>
2019-12-24 01:08:53 +0900

    ガンバッタ

--- ABC/ABC062A.rb
+++ ABC/ABC062A.rb
@@ -1,3 +1,13 @@
 a, b = gets.chomp.split(" ").map(&:to_i)
 
-answer = "No" if a == 2
+array = [[1,3,5,7,8,10,12],[4,6,9,11],[2]]
+
+group_a = 0
+
+array.each_with_index do |group, idx|
+  group_a = idx if group.include?(a)
+end
+
+array[group_a].include?(b) ? answer = "Yes" : answer = "No"
+
+puts answer
